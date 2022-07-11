cefbf13034e4666479c41b925732a0b17646c070
Babin Bohora <bohorababin1@gmail.com>
2022-07-12 01:32:33 +0900

    few pages created

---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h2>This is Blog Page.</h2>
+      <p>
+        These are some of my blogs.
+      </p>
+    </div>
+  )
+}
+
+export default BlogPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h2>
+        Hello, Here is how to contact me.
+      </h2>
+      <p>
+        To connect with me on facebook, <a href="www.facebook.com">babinbohora</a> .
+      </p>
+    </div>
+  )
+};
+
+export default ContactPage;
