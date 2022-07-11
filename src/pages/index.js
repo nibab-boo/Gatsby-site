d69de53baf3f7325e0f34787551f1a91a334b2c2
Babin Bohora <bohorababin1@gmail.com>
2022-07-12 01:16:01 +0900

    basic index remodelled

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <h1>Hello From Index</h1>
+  )
 }
+
+export default IndexPage;
