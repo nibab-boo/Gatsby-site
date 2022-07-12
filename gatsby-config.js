da966dc820e29587fdbfd620f949d65ed87b16dd
Babin Bohora <bohorababin1@gmail.com>
2022-07-12 22:11:34 +0900

    contentful configured

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+ require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
@@ -11,7 +14,15 @@ module.exports = {
     author: "Babin Bohora",
   },
   plugins: [
-    'gatsby-plugin-sass',
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCES_TOKEN,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sass",
     {
       resolve: "gatsby-source-filesystem",
       options: {
