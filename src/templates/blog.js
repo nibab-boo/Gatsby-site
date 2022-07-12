f9758e53837a34a7d16a4614aa0b0a362936de3e
Babin Bohora <bohorababin1@gmail.com>
2022-07-13 01:46:59 +0900

    blog template refactoredc

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,33 +1,58 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 import Layout from "../components/layout";
 
-import { graphql } from "gatsby";
 
 export const query = graphql`
-  query ($slug: String!) {
-    markdownRemark(fields: {slug: {eq: $slug}}) {
-      frontmatter {
-        date
-        title
+  query($slug: String!) {
+    contentfulBlogPost(slug: {eq: $slug })
+    {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            __typename
+            gatsbyImageData
+            placeholderUrl
+          }
+          description
+          title
+        }
       }
-      html
     }
   }
 `
 
 const Blog = (props) => {
 
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        console.log("node", node);
+        const gatsbyImageData = node.data.target
+        if (!gatsbyImageData) {
+          // asset is not an image
+          return null
+        }
+        const url = props.data.contentfulBlogPost.body.references[0].placeholderUrl.replace("%width%", 700).replace("%height%", 500);
+        console.log(url);
+        const alt = props.data.contentfulBlogPost.body.references[0].title;
+
+        return <img alt={alt} src={url}/>
+      },
+    },
+  }
+  // console.log("props", props)
   return (
     <Layout>
-      <div>
-        <h3>{ props.data.markdownRemark.frontmatter.title }</h3>
-        <p>
-          <em>{ props.data.markdownRemark.frontmatter.date }</em>
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}>
-        </div>
-      </div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <h1>{props.data.contentfulBlogPost.publishedDate}</h1>
+      {documentToReactComponents(JSON.parse(props.data.contentfulBlogPost.body.raw), options)}
     </Layout>
   )
 };
